45b8427fcdfea5fb1527913d1feb74e3d4f32fa9
tcyang <tcyang@gm.pu.edu.tw>
2021-05-28 14:32:30 +0800

    2021上機考v3_楊子青

--- app/src/main/java/tw/edu/pu/csim/tcyang/crazyshape/MainActivity.kt
+++ app/src/main/java/tw/edu/pu/csim/tcyang/crazyshape/MainActivity.kt
@@ -12,11 +12,15 @@ import kotlinx.android.synthetic.main.activity_main.*
 public final class MyAppGlideModule : AppGlideModule()
 
 class MainActivity : AppCompatActivity() {
+
+    var Flag:Int = 0
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         Toast.makeText(baseContext, "作者：楊子青", Toast.LENGTH_LONG).show()
+        rndShape()
 
         GlideApp.with(this)
             .load(R.drawable.cover)
@@ -30,5 +34,21 @@ class MainActivity : AppCompatActivity() {
                 return true
             }
         })
+
+        imgNext.setOnClickListener(object:View.OnClickListener{
+            override fun onClick(p0: View?) {
+                rndShape()
+            }
+        })
+    }
+
+    fun rndShape(){
+        Flag = (1..4).random()
+        when (Flag){
+            1->imgNext.setImageResource(R.drawable.circle)
+            2->imgNext.setImageResource(R.drawable.square)
+            3->imgNext.setImageResource(R.drawable.star)
+            4->imgNext.setImageResource(R.drawable.triangle)
+        }
     }
 }
